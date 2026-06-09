1e4ef8700821229387b4acea285be071720e12b2
mdarce765 <murilodarce765@gmail.com>
2026-06-09 11:41:20 -0300

    Protótipo do menu

--- javagotchi/src/main/java/br/com/javagotchi/Main.java
+++ javagotchi/src/main/java/br/com/javagotchi/Main.java
@@ -1,5 +1,7 @@
 package br.com.javagotchi;
 
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -7,11 +9,30 @@ public class Main {
 
 	public static void main(String[] args) {
 		Javagotchi jc = new Javagotchi();
+		Scanner sc = new Scanner(System.in);
 
 		jc.setNome("Izanagi");
 		jc.setEnergia(100);
 
 		System.out.println("Nome = " + jc.getNome());
-	}
 
+		do{
+			System.out.println("""
+							1 - Carregar Javagotchi
+							2 - Criar novo Javagotchi
+							0 - Sair
+							""");
+			System.out.print("Digite a sua opção: ");
+			int opcao = sc.nextInt();
+			switch(opcao){
+					case 0 -> {
+						System.out.println("SAINDO... NÃO SE ESQUEÇA DE NÓS :)");
+						System.exit(0);
+					}
+					//case 1 -> carregarJavagotchi();
+					//case 2 -> criarJavagotchi();
+					default -> System.out.println("OPÇÃO INVÁLIDA");
+				}
+		}while(true);
+	}
 }
